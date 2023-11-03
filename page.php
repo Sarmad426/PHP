e305735813236a3383e7aa43c0d98b7b30744bde
Sarmad rafique <sarmadrafique040@gmail.com>
2023-11-03 12:34:56 +0500

    Front page

---
+++ page.php
@@ -0,0 +1,70 @@
+<?php
+
+$server = 'localhost';
+$username = 'root';
+$pass = '';
+$dbname = 'ecommerce';
+
+$query = mysqli_connect($server,$username,$pass,$dbname);
+
+if($query){
+    echo "Connection Successful";
+}else{
+    echo "Connection Error";
+}
+
+echo "<br>";
+
+$search = "SELECT * FROM `products`";
+
+$result = mysqli_query($query,$search);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+    <style>
+        .card-parent{
+            display:flex;
+            align-items:center;
+            justify-content: start;
+            gap: 10px;
+        }
+        .card{
+            padding: 5px;
+            font-family:'dubai';
+            margin: 30px;
+            background-color: #eee;
+            border-radius: 8px;
+        }
+        .card img{
+            width: 400px;
+        }
+    </style>
+</head>
+<body>
+   <?php
+// Assuming you already have a database connection established
+
+// Fetch data from the database
+// $query = "SELECT id, title, category, image, price FROM products";
+// $result = mysqli_query($connection, $query);
+
+// Display data in cards
+
+while ($row = mysqli_fetch_assoc($result)) {
+    echo '<div class="card-parent">';
+  echo '<div class="card">';
+  echo '<img src="' . $row['image'] . '" alt="' . $row['title'] . '">';
+  echo '<h3>' . $row['title'] . '</h3>';
+  echo '<p class="price">$' . $row['price'] . '</p>';
+  echo '</div>';
+  echo '</div>';
+}
+
+mysqli_close($query);
+?>
+</body>
+</html>
